69cf76067c7e53ecb3c0b9d424c41eb022f4ac30
Rodrigo Plassmann <rodrigo.plassmann@hotmail.com>
2025-10-16 08:34:40 -0300

    Mudança no tamanho das tabelas

--- src/execucao/ParametrosExperimento.java
+++ src/execucao/ParametrosExperimento.java
@@ -4,7 +4,7 @@ public final class ParametrosExperimento {
     private ParametrosExperimento(){}
 
     // Três tamanhos de tabela (ex.: primos próximos de 10^3, 10^4, 10^5)
-    public static final int[] TAMANHOS_TABELA = { 1_009, 10_007, 100_003 };
+    public static final int[] TAMANHOS_TABELA = { 142_867, 1_428_581, 14_285_729 };
 
     // Três tamanhos de dados
     public static final int[] TAMANHOS_DADOS = { 100_000, 1_000_000, 10_000_000 };
