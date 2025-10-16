7a680c2f6754c623f03d02d7f4e0b70278165b2a
Rodrigo Plassmann <rodrigo.plassmann@hotmail.com>
2025-10-16 18:47:52 -0300

    Dados menores usados e arquivos gerados

--- src/execucao/ParametrosExperimento.java
+++ src/execucao/ParametrosExperimento.java
@@ -4,10 +4,10 @@ public final class ParametrosExperimento {
     private ParametrosExperimento(){}
 
     // Três tamanhos de tabela (ex.: primos próximos de 10^3, 10^4, 10^5)
-    public static final int[] TAMANHOS_TABELA = { 1_009, 10_007, 100_003};
+    public static final int[] TAMANHOS_TABELA = { 1_000, 10_000, 100_000};
 
     // Três tamanhos de dados
-    public static final int[] TAMANHOS_DADOS = { 100_000, 1_000_000, 10_000_000 };
+    public static final int[] TAMANHOS_DADOS = { 100_000, 500_000, 1_000_000 };
 
     // Semente fixa para reprodutibilidade
     public static final long SEMENTE = 12345L;
